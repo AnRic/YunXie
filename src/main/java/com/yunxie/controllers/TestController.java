32f67fdc620680285f7a8562f3768559bcdec2c3
wuxiaohu <wuxh@tourage.cn>
2018-05-07 15:24:46 +0800

    wxh - 新增页面跳转

--- src/main/java/com/yunxie/controllers/TestController.java
+++ src/main/java/com/yunxie/controllers/TestController.java
@@ -4,14 +4,15 @@ import com.yunxie.models.Test;
 import com.yunxie.repositories.TestRepository;
 import com.yunxie.repositories.TestXMLRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by WXH on 2017/11/20.
  */
 
-@RestController
+@Controller
 @RequestMapping("test")
 public class TestController {
 
@@ -22,6 +23,7 @@ public class TestController {
     TestXMLRepository testXMLRepository;
 
     @RequestMapping("test")
+    @ResponseBody
     public Test test(){
         Test test = new Test();
         test.setId(Long.valueOf(1));
@@ -31,15 +33,24 @@ public class TestController {
     }
 
     @RequestMapping("getTest")
+    @ResponseBody
     public Test getTest(Long id){
         return testRepository.getById(id);
 
     }
 
     @RequestMapping("getTest1")
+    @ResponseBody
     public Test getTest1(Integer id){
         return testXMLRepository.selectByPrimaryKey(id);
 
+    }
+
+
+    //跳转页面
+    @RequestMapping("index")
+    public String index(){
+        return "test/index";
     }
 
 }
